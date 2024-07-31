1e0679bca9c888e8b20c3e92ac1c5f9ef4f11eb2
joao victor <joaovprudencio13@gmail.com>
2024-07-31 12:08:27 -0300

    criando cliente

--- src/db/conexao.ts
+++ src/db/conexao.ts
@@ -1,23 +1,23 @@
- import  { Client } from 'pg';
- import dotenv from 'dotenv';
+import { Client } from "pg";
+import dotenv from "dotenv";
 
- dotenv.config();
+dotenv.config();
 
- const banco = process.env.DB; 
- const senha = process.env.SENHA; 
- const user = process.env.USERNAME; 
+const banco = process.env.DB;
+const senha = process.env.SENHA;
+const user = process.env.USERNAME;
 
- const conString: string = process.env.DATABASE_URL  ||` postgres://${user}:${senha}@localhost/${banco} ;
+const conString: string =
+  process.env.DATABASE_URL || `postgres://${user}:${senha}@localhost/${banco}`;
 
- const client: Client = new Client({
-    connectionString:conString,
- });
+const client: Client = new Client({
+  connectionString: conString,
+});
 
- client.connect((err: Error)=>{
-    if(err) {
-        console.log("erro ao conectar" , err.stack);
-    }
+client.connect((err: Error) => {
+  if (err) {
+    console.log("erro ao conectar", err.stack);
+  }
 
-    console.log('Connected to the database');
-
- });
+  console.log("Connected to the database");
+});
---
+++ src/models/Cliente.ts
@@ -0,0 +1,20 @@
+export class Cliente {
+  nome: string;
+  email: string;
+  senha: string;
+  numero: string;
+
+  constructor(nome: string, email: string, senha: string, numero: string) {
+    this.email = email;
+    this.nome = nome;
+    this.senha = senha;
+    this.numero = numero;
+  }
+
+
+
+
+
+
+  
+}
